f7b03ff97d11427c03a6810b604d3862f2ec4a13
Kaio Rodrigues Saravy <kaiorodriguessaravy@gmail.com>
2023-05-18 21:20:46 -0300

    first commit

---
+++ src/main/java/com/DesafioDigix/Desafio/Digix/DesafioDigixApplication.java
@@ -0,0 +1,13 @@
+package com.DesafioDigix.Desafio.Digix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDigixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDigixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DesafioDigix/Desafio/Digix/DesafioDigixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DesafioDigix.Desafio.Digix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDigixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
